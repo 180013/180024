e30769c32fabedb667d87c8cc675c1edb5df77af
JulietaRodriguezEspiricueta <180024@upslp.edu.mx>
2020-10-06 11:13:54 -0500

    Add files via upload

---
+++ AFD/180024-afd.pl
@@ -0,0 +1,115 @@
+#!/usr/bin/perl
+
+ # ***************************************************************************
+ #  Nombre:		Julieta Rodríguez Espiricueta
+ #  Carrera:		Ingeniería en Tecnologías de la Información (ITI)
+ #  Profesor:		Juan Carlos González Ibarra
+ #  Institución:	Universidad Politécnica de San Luis Potosí
+ #  Matrícula:		180024
+ #
+ #  Descripción:	Programa que recrea el funcionamiento de un
+ #				autómata finito determinista (AFD)
+ #
+ #****************************************************************************
+
+use strict;                     
+use warnings;
+use feature "say";        		# |  Característica de Perl que imprime un salto de línea automático
+
+use vars qw ( $simbolo );	# |  Variable globales
+use vars qw ( $fin );		# |  Variable globales
+
+$simbolo = undef;
+$fin = undef;
+
+sub caracter {
+	my ( $character ) = @_;
+	
+	$simbolo = "";
+	$fin = "";
+	
+	if ($character =~ /^\d+$/) {			# |  El comando \d reconoce los dígitos
+		$simbolo = " Digito ";
+		return 0;
+	} else {
+		if ( $character =~ /\+|\-/ or $character eq "*" or $character eq "/") {
+			$simbolo = "Operador";
+			return 1;
+		}else{
+			if ( $character eq $fin ) {
+				return 2;
+			}
+		}
+		#say ( "El caracter $character no es valido\n" );
+		#exit ();
+		
+	}	
+}
+
+#  Función para el encabezado de la tabla
+sub encabezado {
+	say ("|  Edo. Actual |Caracter |  Simbolo  |Edo. Siguiente |");
+	&cuerpo;
+}
+
+#  Función que guarda cada valor despues de encontrarlo en un ciclo
+sub contenido {
+	my ( $estadosig, $character, $simbolo, $estado ) = @_;
+	say ("|     ",$estadosig,"      |  ",$character,"    |",$simbolo," |     ",$estado,"       |");
+	&cuerpo;
+}
+
+#  Función que muestra la línea que se repetirá cada vez que la mandemos llamar
+sub cuerpo {
+	say ("+--------------+---------+-----------+---------------+");
+}
+
+#  MAIN
+#  Este es la tabla de transiciones del automata AFD creado
+my @tabla= ([1,"E","E"],["E",2,"E"],[3,"E","E"],["E","E","A"]);		# |  Toma los valores del return
+my $estado = 0;
+
+say ("+-------------------------------------+");
+say ("|    Ingrese una cadena a evaluar:    |");
+say ("+-------------------------------------+");
+	
+my $cadena = <STDIN>;			# |  Captura de la cadena por teclado
+chomp $cadena;					# |  Se elimina la reserva el símbolo de \n
+my @cadena = split (//, $cadena);		# |  La cadena se convierte en un arreglo
+
+&cuerpo;
+&encabezado;
+
+#  Ciclo para recorrer la cadena
+for my $character (@cadena) {
+	my $estadosig = $estado;
+	
+	#  Llamada a la función para saber si es un caracter válido
+	my $charcaracter = &caracter ( $character );
+	
+	#  Se guarda el valor obtenido en la tabla según las coordenadas recibidas
+	$estado = $tabla [$estado][$charcaracter];
+	
+	#  Si el valor obtenido es una E, imprimimos "Cadena no válida"
+	if ($estado eq "E") {
+		say ("|     ",$estadosig,"      |  ",$character,"    |",$simbolo," |     ",$estado,"       |");
+		&cuerpo;
+		say "|              Cadena no valida :(                   |";
+		say ("+----------------------------------------------------+");
+		exit ();
+	}
+	&contenido ($estadosig, $character, $simbolo, $estado);
+}
+
+#  Si al concluir el estado no es 3, que es el de aceptación, se imprime "Cadena no válida"    
+if ( $estado != 3 ) {
+        say "|              Cadena no valida :(                   |";
+        say ("+----------------------------------------------------+");
+}
+
+#  Si el estado es 3, entonces es una cadena de aceptación
+if ( $estado == 3 ) {
+    say ("|     ",$estado,"      |         |Fin cadena |               |");
+    &cuerpo;
+    say ("|                Cadena valida                       |\n+----------------------------------------------------+");
+}
---
+++ AFD/README.md
@@ -0,0 +1,78 @@
+# Programa en Perl de un AFD
+## Datos personales
+- **Nombre:**       Julieta Rodríguez Espiricueta  
+- **Carrera:**      Ingeniería en Tecnologías de la Información (ITI)  
+- **Profesor:**     Juan Carlos González Ibarra  
+- **Institución:**	Universidad Politécnica de San Luis Potosí  
+- **Matrícula:**    180024
+
+
+## Descripción
+Programa que recrea el funcionamiento de un Autómata Finito Determinista (AFD), en particular el reconocimiento de cadenas. Se valida un entrada similar a 1+2, reconociendo  
+patrones en la cadena ingresada para determinar si esta es válida o inválida.  
+
+
+## Sintaxis
+En Perl, es posible utilizar los comandos para expresiones regulares que el propio lenguaje ya tiene por defecto. Entre estas expresiones se encuentra "=~":
+    "Hello World" =~ /World/;		# |  Se encuentra el patrón en la cadena  
+
+Además, se tuvieron que inicializar las valiables globales como "undef", es decir, no se le asigna un valor a una variable.
+
+#### Declaración de variables globales
+    use vars qw ( $simbolo );
+    use vars qw ( $fin );
+    $simbolo = undef;
+    $fin = undef;
+De esta manera, las variables globales inician sin un valor asignado al iniciar el programa.
+
+#### Comandos
+    $character =~ /\d+$/					# |  Reconoce si hay un dígito en la cadena
+    
+    $character =~ /\+|\-/ or $character eq "*" or $character eq "/"	# |  Reconoce si hay un operador en la cadena
+
+#### Función base para la generación de la tabla
+```
+my @tabla= ([1,"E","E"],["E",2,"E"],[3,"E","E"],["E","E","A"]);
+my $estado = 0;
+
+#  Ciclo para recorrer la cadena
+for my $character (@cadena) {
+	my $estadosig = $estado;
+	
+	#  Llamada a la función para saber si es un caracter válido
+	my $charcaracter = &caracter ( $character );
+	
+	#  Se guarda el valor obtenido en la tabla según las coordenadas recibidas
+	$estado = $tabla [$estado][$charcaracter];
+	
+	#  Si el valor obtenido es una E, imprimimos "Cadena no válida"
+	if ($estado eq "E") {
+		say ("|     ",$estadosig,"      |  ",$character,"    |",$simbolo," |     ",$estado,"       |");
+		&cuerpo;
+		say "|              Cadena no valida :(                   |";
+		say ("+----------------------------------------------------+");
+		exit ();
+	}
+	&contenido ($estadosig, $character, $simbolo, $estado);
+}
+
+#  Si al concluir el estado no es 3, que es el de aceptación, se imprime "Cadena no válida"    
+if ( $estado != 3 ) {
+        say "|              Cadena no valida :(                   |";
+        say ("+----------------------------------------------------+");
+}
+
+#  Si el estado es 3, entonces es una cadena de aceptación
+if ( $estado == 3 ) {
+    say ("|     ",$estado,"      |         |Fin cadena |               |");
+    &cuerpo;
+    say ("|                Cadena valida                       |\n+----------------------------------------------------+");
+}
+```
+
+
+## Problemas y soluciones al programar  
+Al momento de tratar de encontrar un patrón que reconociera los dígitos en la cadena, el programa no diferencía letras de números, por lo que si se ingresaba la  
+cadena e+4 se tomaba como válida. Incluso se llegó a implementar el módulo *Regexp::Common* y el *Scalar::Util qw(looks_like_number)*, pero seguía sin  
+distinguir letras de números.  
+Debido a esto, se optó por utilizar `$character =~ /\d+$/`, donde `/\d/` en teoría reconoce los números.  
